bd6e535bb4c75d9acc3ce92d76b439386fb369f1
kanziw <kanziwoong@gmail.com>
2018-05-26 14:37:27 +0900

    Add optimization test for tail recursion

---
+++ test/optimization.spec.ts
@@ -0,0 +1,25 @@
+import RepeatingTaskManager from '../'
+import { expect } from 'chai'
+import { delay, onError } from './common'
+
+describe('[ Optimization ]', function () {
+  this.timeout(11000)
+  let rtm: RepeatingTaskManager
+  beforeEach(() => { rtm = new RepeatingTaskManager() })
+  afterEach(() => rtm.clearAll())
+
+  it('Is it tail recursive? => stack trace does not have caller function name.', async () => {
+    const task = 'TASK'
+    let ret = 0
+    const checkStackTraceFn = () => {
+      const lastErrorStack = new Error().stack as string
+      const split = lastErrorStack.split('at ')
+      expect(split.filter(str => str.includes('checkStackTraceFn')).length).eql(1)
+      ret++
+    }
+
+    rtm.register(task, 1, checkStackTraceFn, { onError })
+    await delay(100)
+    expect(ret).above(10)
+  })
+})
