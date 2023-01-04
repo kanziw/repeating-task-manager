0ea249d60f3e8576a5fe9dcb3a2de9e549de224b
Jiwoong Jung <kanziwoong@gmail.com>
2023-01-04 18:08:35 +0900

    Add lint ci

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  env: {
+    es2021: true,
+    node: true
+  },
+  extends: 'standard-with-typescript',
+  overrides: [
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: './tsconfig.eslint.json'
+  },
+  rules: {
+    '@typescript-eslint/strict-boolean-expressions': 'off'
+  }
+}
