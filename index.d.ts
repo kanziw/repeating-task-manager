c8c3bd1b17d9e01b08ff6b9567f391c7825753f7
Jiwoong Jung <kanziwoong@gmail.com>
2023-01-04 15:51:06 +0900

    Update dependencies and build .d.ts

---
+++ index.d.ts
@@ -0,0 +1,26 @@
+export interface TaskFunction {
+    (options: RepeatingTaskOptions): any;
+}
+export interface RepeatingTaskOptions {
+    readonly isRegister: boolean;
+}
+export interface RegisterOptions {
+    readonly onError?: onErrorFunction;
+}
+export interface onErrorFunction {
+    (error: Error): void;
+}
+export declare type TaskID = string;
+export default class RepeatingTaskManager {
+    private dicFn;
+    private dicClearFn;
+    private dicPause;
+    register(taskId: TaskID, interval: number, taskFunction: TaskFunction, { onError }?: RegisterOptions): void;
+    execute: (taskId: TaskID, options?: RepeatingTaskOptions | undefined) => Promise<any>;
+    clear(taskId: TaskID): void;
+    clearAll(): void;
+    pause(taskId?: TaskID): void;
+    resume(taskId?: TaskID): void;
+    private doTaskAfter;
+}
+export declare function getDuplicatedTaskKeyError(taskId: TaskID): Error;
